d7b9220ca393bdcc6a2d4085704189216885983a
Vasilis Alepopoulos <vasilisalepopoulos@gmail.com>
2019-09-22 12:31:36 +0300

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -4,14 +4,14 @@ from selenium.common.exceptions import NoSuchElementException
 import mysql.connector
 
 def rgb_to_hex(rgb):
-    print(rgb)
     heximal = Color.from_string(rgb).hex
     return heximal; 
 def isCorrect(color):
+    correct_answer=""
     if (color =='#00ff00'):
         correct_answer=answer_text[i]
-        return(correct_answer)
-    
+    return(correct_answer)
+
  
 # creates an firefox browser instance
 driver = webdriver.Firefox(executable_path="C:\\Users\\jedic\\Desktop\\diktia til\\geckodriver.exe")
@@ -22,7 +22,7 @@ driver.get(url)
 
 #connects to DB
 connection = mysql.connector.connect(host='localhost',
-                                     database = 'diktia_til',
+                                     database = 'exams',
                                      user = 'root',
                                      password = 'ac0b3passGGsmokescreen',
                                      )
@@ -35,6 +35,7 @@ for j in range (99):
     correct_answer="";
     #scrapes the question
     question_1_text = driver.find_element_by_css_selector("#myCanvasAnsQ > div:nth-child(6)").text
+    
 
     
     #chooses the first answer in order to show if it is correct or false
@@ -48,27 +49,24 @@ for j in range (99):
         try:
             answer_selector="#myCanvasAns%d"%(i+1)
             answer_text.append(driver.find_element_by_css_selector(answer_selector+answer_div).text)
-            #gets first answer's background color in rgb and transforms it in hex
+        except NoSuchElementException as exception:
+            answer_text.append("NULL")
+        try:
+            #gets answer's background color in rgb and transforms it in hex
             color = rgb_to_hex(driver.find_element_by_css_selector(answer_selector+bc_div).value_of_css_property('background-color'))
             correct_answer=isCorrect(color)
-            
-        except NoSuchElementException as exception:
-            print("doesn't exist")   
-
-
-    #inserts into database questions and answers   
-    query = 'INSERT INTO questions (number, question) values (%d,"%s")' %(i+1,question_1_text)
+        except NoSuchElementException as exception:    
+            print("doesn't exist")
+    
+    query = 'INSERT INTO answers (answers_id, answer1, answer2, answer3, answer4)VALUES (%d, "%s","%s","%s","%s")' %(j+1, answer_text[0], answer_text[1], answer_text[2], answer_text[3])
     cursor.execute(query)
-    query = 'INSERT INTO answers (answer1, answer2, answer3, answer4, correct_answer, number) values ("%s","%s","%s","%s","%s",%d)' %(answer_text[0],
-                                                                                                                      answer_text[1],
-                                                                                                                      answer_text[2],
-                                                                                                                      answer_text[3],
-                                                                                                                      correct_answer,
-                                                                                                                      j+1)
-    #go to the next page
-    driver.find_element_by_css_selector("#myCanvasBtn > div:nth-child(1)").click()
-    cursor.execute(query)
+    query = 'INSERT INTO correct_answer(cAnswer_id,text)VALUES (%d,"%s")'%(j+1,correct_answer)
+    cursor.execute(query) 
 
+    query = 'INSERT INTO question (answers_id,cAnswer_id,question_id,text)VALUES (%d, %d, %d,"%s")' %(j+1, j+1, j+1, question_1_text)
+    cursor.execute(query)
+    driver.find_element_by_css_selector('#myCanvasBtn > div:nth-child(1)').click()
+    
 
  
 
